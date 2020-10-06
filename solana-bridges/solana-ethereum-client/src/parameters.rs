127fbdbea13182b6f6a67a6cd7e24e7e902b4807
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-10-06 18:27:48 +0100

    Store highest block number seen

--- solana-bridges/solana-ethereum-client/src/parameters.rs
+++ solana-bridges/solana-ethereum-client/src/parameters.rs
@@ -1,1 +1,9 @@
+use std::mem;
+
 pub const HEADER_HISTORY_SIZE: usize = 100;
+
+pub const COUNT_OFFSET: usize = 0;
+pub const COUNT_SIZE: usize = mem::size_of::<u64>();
+pub const HEIGHT_OFFSET: usize = COUNT_SIZE;
+pub const HEIGHT_SIZE: usize = mem::size_of::<u64>();
+pub const BLOCKS_OFFSET: usize = COUNT_SIZE + HEIGHT_SIZE;
--- solana-bridges/solana-ethereum-client/src/processor.rs
+++ solana-bridges/solana-ethereum-client/src/processor.rs
@@ -3,6 +3,7 @@
 use crate::{
     eth::*,
     instruction::*,
+    parameters::*,
     types::*,
 };
 
@@ -16,7 +17,6 @@ use solana_sdk::{
     program_pack::{Pack},
     pubkey::Pubkey,
 };
-use std::mem;
 
 pub fn process_instruction<'a>(
     program_id: &Pubkey,
@@ -58,16 +58,31 @@ pub fn process_instruction<'a>(
     return Ok(());
 }
 
+pub fn read_block_count(data: &[u8]) -> u64 {
+    let count_src = array_ref![data, COUNT_OFFSET, COUNT_SIZE];
+    return u64::from_le_bytes(*count_src);
+}
+pub fn read_block_height(data: &[u8]) -> u64 {
+    let count_src = array_ref![data, HEIGHT_OFFSET, HEIGHT_SIZE];
+    return u64::from_le_bytes(*count_src);
+}
+
+pub fn write_block_count(data: &mut [u8], new_count: u64) {
+    let count_dst = array_mut_ref![data, COUNT_OFFSET, COUNT_SIZE];
+    *count_dst = new_count.to_le_bytes();
+}
+pub fn write_block_height(data: &mut [u8], new_height: u64) {
+    let height_dst = array_mut_ref![data, HEIGHT_OFFSET, HEIGHT_SIZE];
+    *height_dst = new_height.to_le_bytes();
+}
+
 pub fn read_prev_block(account: &AccountInfo) -> Result<BlockHeader, ProgramError> {
     let data = account.try_borrow_mut_data()?;
 
-    const COUNT_SIZE: usize = mem::size_of::<usize>();
-    let count_src = array_ref![data, 0, COUNT_SIZE];
-
-    match usize::from_le_bytes(*count_src) {
+    match read_block_count(&data) {
         0 => return Err(CustomError::NoParentBlock.to_program_error()),
         count => {
-            let header_src = array_ref![data, COUNT_SIZE + BlockHeader::LEN * (count - 1), BlockHeader::LEN];
+            let header_src = array_ref![data, BLOCKS_OFFSET + BlockHeader::LEN * ((count - 1) as usize), BlockHeader::LEN];
             let header = BlockHeader::unpack_from_slice(header_src)?;
             return Ok(header);
         }
@@ -77,12 +92,11 @@ pub fn read_prev_block(account: &AccountInfo) -> Result<BlockHeader, ProgramErro
 pub fn write_new_block(account: &AccountInfo, header: BlockHeader) -> Result<(), ProgramError> {
     let mut data = account.try_borrow_mut_data()?;
 
-    const COUNT_SIZE: usize = mem::size_of::<usize>();
-    let count_src = array_mut_ref![data, 0, COUNT_SIZE];
-    let count = usize::from_le_bytes(*count_src);
-    *count_src = (count + 1).to_le_bytes();
+    let count = read_block_count(&data);
+    write_block_count(&mut data, count+1);
+    write_block_height(&mut data, header.number);
 
-    let header_dst = array_mut_ref![data, COUNT_SIZE + BlockHeader::LEN * count, BlockHeader::LEN];
+    let header_dst = array_mut_ref![data, BLOCKS_OFFSET + BlockHeader::LEN * (count as usize), BlockHeader::LEN];
     header.pack_into_slice(header_dst);
     return Ok(());
 }
