9c2b3177d8c65dfcb3ff203409d8cdb12442760d
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-10-06 16:57:56 +0100

    Show test time

--- solana-bridges/solana-ethereum-client/watch.sh
+++ solana-bridges/solana-ethereum-client/watch.sh
@@ -1,1 +1,1 @@
-nix-shell ../../default.nix -A shells.target-x86 --run "CARGO_TARGET_DIR='target-x86' cargo watch -c -x 'test --features program'"
+nix-shell ../../default.nix -A shells.target-x86 --run "CARGO_TARGET_DIR='target-x86' cargo watch -c -x 'test --features program -- -Z unstable-options --report-time'"
