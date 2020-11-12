c9d3309285b798d92b112726d76514b1731b0a5d
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-11-12 21:02:18 +0000

    Fix sha256

--- default.nix
+++ default.nix
@@ -218,7 +218,7 @@ let
   ethereum-client-src = gitignoreSource ./solana-bridges/ethereum-client;
 
   # Cargo hash must be updated when Cargo.lock file changes.
-  ethereum-client-dep-sha256 = "0j4lfp20rvdnx8gnp2qqi789z3mzlkmzqpijpgy93xy4q3y1z5ar";
+  ethereum-client-dep-sha256 = "1lgp9imanv9nywmxacq51937hzxrw8mlqvyv1wwbqkfpyci9i6mq";
   ethereum-client-dep-srcs = nixpkgs.rustPlatform.fetchCargoTarball {
     name = "ethereum-client";
     src = ethereum-client-src;
